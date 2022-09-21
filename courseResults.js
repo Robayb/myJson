deeeb8f22826f3ed0cb76bb4e7f680831f91bc99
Robayb <Robertj63@gmail.com>
2022-09-21 15:46:24 -0400

    Create courseResults.js

---
+++ courseResults.js
@@ -0,0 +1,116 @@
+{
+   "Labs": [
+      {
+         "Class Type": "Any",
+         "MarketSize": "Any",
+         "Days": 1,
+         "Additional Days": 0,
+         "Quantity": 1,
+         "Max Students Per Lab": 12,
+         "Allocated Students": 12,
+         "OverSeatMax": false,
+         "Price": 685,
+         "Unit of Measure": "Per Lab",
+         "Extended Price": 685
+      }
+   ],
+   "Materials": [
+      {
+         "Class Type": "Any",
+         "MarketSize": "Any",
+         "Days": 1,
+         "Additional Days": 0,
+         "Quantity": 1,
+         "Max Students Per Lab": 12,
+         "Allocated Students": 12,
+         "OverSeatMax": false,
+         "Price": 100,
+         "Unit of Measure": "Per Student",
+         "Extended Price": 1200
+      }
+   ],
+   "Instructor": [
+      {
+         "Class Type": "Any",
+         "MarketSize": "Any",
+         "Days": 1,
+         "Additional Days": 0,
+         "Quantity": 1,
+         "Max Students Per Lab": 12,
+         "Allocated Students": 12,
+         "OverSeatMax": false,
+         "Price": 100,
+         "Unit of Measure": "Per Day",
+         "Extended Price": 1200
+      }
+   ],
+   "Instructor_AdditionalDays": [
+      {
+         "Class Type": "Any",
+         "MarketSize": "Any",
+         "Days": 1,
+         "Additional Days": 0,
+         "Quantity": 1,
+         "Max Students Per Lab": 12,
+         "Allocated Students": 12,
+         "OverSeatMax": false,
+         "Price": 1000,
+         "Unit of Measure": "Per Day",
+         "Extended Price": 0
+      }
+   ],
+   "T&E": [
+      {
+         "Class Type": "Virtual",
+         "MarketSize": "Any",
+         "Days": 1,
+         "Additional Days": 0,
+         "Quantity": 1,
+         "Max Students Per Lab": 12,
+         "Allocated Students": 12,
+         "OverSeatMax": false,
+         "Price": 0,
+         "Unit of Measure": "Per Day",
+         "Extended Price": 0
+      },
+      {
+         "Class Type": "Onsite",
+         "MarketSize": "Small",
+         "Days": 1,
+         "Additional Days": 0,
+         "Quantity": 1,
+         "Max Students Per Lab": 12,
+         "Allocated Students": 12,
+         "OverSeatMax": false,
+         "Price": 500,
+         "Unit of Measure": "Per Day",
+         "Extended Price": 2000
+      },
+      {
+         "Class Type": "Onsite",
+         "MarketSize": "Medium",
+         "Days": 1,
+         "Additional Days": 0,
+         "Quantity": 1,
+         "Max Students Per Lab": 12,
+         "Allocated Students": 12,
+         "OverSeatMax": false,
+         "Price": 500,
+         "Unit of Measure": "Per Day",
+         "Extended Price": 2000
+      },
+      {
+         "Class Type": "Onsite",
+         "MarketSize": "Large",
+         "Days": 1,
+         "Additional Days": 0,
+         "Quantity": 1,
+         "Max Students Per Lab": 12,
+         "Allocated Students": 12,
+         "OverSeatMax": false,
+         "Price": 500,
+         "Unit of Measure": "Per Day",
+         "Extended Price": 2000
+      }
+   ]
+}
